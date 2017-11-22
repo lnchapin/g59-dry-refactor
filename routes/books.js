e6c6cc3079667ad3543593a8a7d78fe9a4dfb1c1
Lindsay Chapin <lnchapin@gmail.com>
2017-11-22 15:01:50 -0700

    Refactor

--- routes/books.js
+++ routes/books.js
@@ -2,106 +2,92 @@ const knex = require('../lib/knex')
 const express = require('express')
 const router = express.Router()
 
+function checkInput({id}) {
+  if (!id || Number.isNaN(id)) {
+    throw new Error('Invalid ID')
+  }
+  return id
+}
+
+function getOneBook(id) {
+  return knex('books').where('id', id).first()
+}
+
+function update(book) {
+  return knex('books').update({
+    title: book.title,
+    author: book.author,
+    genre: book.genre,
+    description: book.description,
+    cover_url: book.coverUrl
+  }, '*').where('id', book.id)
+}
+
+function getOne(book) {
+  return knex('books').insert({
+    title: book.title,
+    author: book.author,
+    genre: book.genre,
+    description: book.description,
+    cover_url: book.coverUrl
+  }, '*')
+
+}
+
 router.get('/', (req, res, next) => {
-  knex('books')
-  .select('id', 'title', 'author', 'genre', 'description', 'cover_url as coverUrl', 'created_at as createdAt', 'updated_at as updatedAt')
-  .orderBy('title', 'ASC')
-  .then(data => res.send(data))
-  .catch(err => res.status(500).send(err))
+  knex('books').select('*').orderBy('title', 'ASC').then(data => res.send(data)).catch(err => res.status(500).send(err))
 })
 
 router.get('/:id', (req, res, next) => {
-  // make sure that id is a number,
-  // if it is not a number, send a 400 (Bad Request)
-  if (!req.params.id || Number.isNaN(req.params.id)) {
-    throw new Error('Invalid ID')
-  }
-
-  knex('books')
-  .select('id', 'title', 'author', 'genre', 'description', 'cover_url as coverUrl', 'created_at as createdAt', 'updated_at as updatedAt')
-  // .where('id', req.params.id)
-  .first()
+  checkInput(req.params)
+  getOneBook(req.params.id)
   .then((data) => {
     res.send(data)
-  })
-  .catch(err => {
+  }).catch(err => {
     res.status(500).send(err)
   })
 })
 
 router.post('/', (req, res, next) => {
-  // Make sure that the information that you want is in the request body.
-  knex('books')
-    .insert({
-      title: req.body.title,
-      author: req.body.author,
-      genre: req.body.genre,
-      description: req.body.description,
-      cover_url: req.body.coverUrl,
-    }, '*')
-    .then(([bookId]) => {
-      res.send({bookId})
-    })
-    .catch(err => {
-      next(err)
-    })
+  getOne(req.body).then(([bookId]) => {
+    res.send({bookId})
+  }).catch(err => {
+    next(err)
+  })
 })
 
 router.put('/:id', (req, res, next) => {
-  // Make sure that id is a number
-  if (!req.params.id || Number.isNaN(req.params.id)) {
-    throw new Error('Invalid ID')
+return Promise.resolve(checkInput(req.params))
+.then(getOneBook)
+.then(book => {
+  if (!book || book.length === 0) {
+    return res.status(404).send({message: 'Not Found'})
   }
-
-  knex('books')
-    .where('id', req.params.id)
-    .first()
-    .then(book => {
-      if (!book) {
-        return next()
-      }
-
-      return knex('books')
-        .update({
-          title: req.body.title,
-          author: req.body.author,
-          genre: req.body.genre,
-          description: req.body.description,
-          cover_url: req.body.coverUrl,
-        }, '*')
-        .where('id', req.params.id)
-    })
-    .then(books => {
-      if (!books) { return {message: 'nothing found for given id'} }
-      const newObj = {
-        id: books[0].id,
-        title: books[0].title,
-        author: books[0].author,
-        genre: books[0].genre,
-        description: books[0].description,
-        coverUrl: books[0].cover_url,
-      }
-      res.send(newObj)
-    })
-    .catch(err => {
-      next(err)
-    })
+  return req.body
+})
+.then(update)
+.then(books => {
+  const newObj = {
+    id: books[0].id,
+    title: books[0].title,
+    author: books[0].author,
+    genre: books[0].genre,
+    description: books[0].description,
+    coverUrl: books[0].cover_url
+  }
+  res.send(newObj)
+}).catch(err => {
+  next(err)
+})
 })
 
 router.delete('/:id', (req, res, next) => {
-  let book = null
+let book = null
+let id = checkInput(req.params)
 
-  if (!req.params.id || Number.isNaN(req.params.id)) {
-    throw new Error('Invalid ID')
-  }
-
-  knex('books')
-    .del()
-    .where('id', req.params.id)
-    .catch(err => res.status(500).send(err))
-    .then(data => {
-      res.status(200).send({deleted: data})
-    })
+knex('books').del().where('id', id).catch(err => res.status(500).send(err)).then(data => {
+  res.status(200).send({deleted: data})
+})
 })
 
 module.exports = router
